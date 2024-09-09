92d72e135d24cca70baf5e4d778a2e27f8f10931
Yash <yashkhonde222@gmail.com>
2024-09-09 20:25:47 +0530

    in views.py added logic to save data inside database using contact.html page and models.py file

--- iCoder/home/views.py
+++ iCoder/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from home.models import Contact
 
 # Create your views here.
 from django.shortcuts import render, HttpResponse
@@ -11,4 +12,14 @@ def about(request):
     return render(request, "home/about.html")
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        print(name,email,phone,content)
+        contact = Contact(name = name, email = email, phone = phone, content = content)
+        contact.save()
+
     return render(request, "home/contact.html")
+    
