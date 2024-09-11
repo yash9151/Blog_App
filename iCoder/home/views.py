fc086690e718bbfdb79b51b651774dc6a32c6f09
Yash <yashkhonde222@gmail.com>
2024-09-11 17:45:07 +0530

    Added validations in views.py  signup method

--- iCoder/home/views.py
+++ iCoder/home/views.py
@@ -58,7 +58,16 @@ def handleSignup(request):
         pass2 = request.POST['pass2']
 
         # Check for errorneous inputs
-        
+        if len(username) > 10:
+            messages.error(request, "Username must be under 10 characters")
+            return redirect("home")
+        if not username.isalnum():
+            messages.error(request, "Username must be alphanumeric")
+            return redirect("home")
+        if pass1!= pass2:
+            messages.error(request, "Password do not match")
+            return redirect("home")
+
         # Create the user
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
