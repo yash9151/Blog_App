a6c12b3c6f36257ddcf9ce847782de2235b5e82d
Yash <yashkhonde222@gmail.com>
2024-09-27 16:44:28 +0530

    added BlogComment method in models.py to add comments , addedthis routing api to urls.py of blog app and added form in UI of blogPost.html

--- iCoder/blog/urls.py
+++ iCoder/blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.blogHome, name="blogHome"),
+    # API to post a comment
+    path("postComment", views.postComment, name="postComment"),
     path('<str:slug>/', views.blogPost, name="blogPost"),
 ]
